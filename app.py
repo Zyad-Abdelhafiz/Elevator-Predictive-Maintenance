d75c738a79ce30d15dfffb20d156fc023a35fa00
Zyad <96571298+Zyad-Abdelhafiz@users.noreply.github.com>
2024-07-25 13:35:19 +0000

    app

--- app.py
+++ app.py
@@ -1,34 +1,74 @@
 from flask import Flask, request, jsonify
-import tensorflow as tf
+from flask_cors import CORS
+from flask_restful import Api, Resource
+import pickle
+import pandas as pd
+import json
 import numpy as np
 import pandas as pd
+import os
 
-# Load the model
-model = tf.keras.models.load_model('my_model.keras')
-
-# Initialize Flask app
 app = Flask(__name__)
+cors = CORS(app, resources={r"*": {"origins": "*"}})
+api = Api(app)
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    try:
-        # Get JSON data from the request
-        data = request.get_json()
-        
-        # Convert JSON data to DataFrame or the format required by your model
-        df = pd.DataFrame([data])
+pkl_filename = "my_model.pkl"
+with open(pkl_filename, 'rb') as f_in:
+        model = pickle.load(f_in)
 
-        # Make predictions
-        predictions = model.predict(df)
-        
-        # Convert predictions to a list (or format as needed)
-        response = predictions.tolist()
-        
-        # Return the predictions as JSON
-        return jsonify({'predictions': response})
+ 
 
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+def predict_mpg(data):
+    # Convert JSON data to DataFrame
+    df = pd.DataFrame(data)
+    
+    
+    # Convert DataFrame to NumPy array and reshape
+    input_data = df.values.reshape((1, 50, 3)) 
+    
+    # Get prediction probabilities
+    y_pred_prob = model.predict(input_data)
+    
+    # Apply threshold to get binary predictions
+    y_pred = (y_pred_prob > 0.5).astype("int32")
+    
+    # Interpret the prediction result
+    if y_pred[0] == 0:
+        return 'No Need Maintenance'
+    elif y_pred[0] == 1:
+        return 'Need Maintenance'
+    else:
+        return 'Unknown prediction result'
+
+
+class Test(Resource):
+    def get(self):
+        return 'Welcome to Test App API!'
+
+    def post(self):
+        try:
+            value = request.get_json()
+            if value:
+                return {'Post Values': value}, 201
+            return {"error": "Invalid format."}
+        except Exception as error:
+            return {'error': str(error)}
+
+class GetPredictionOutput(Resource):
+    def get(self):
+        return {"error": "Invalid Method."}
+
+    def post(self):
+        try:
+            data = request.get_json()
+            # Ensure the input data is in the correct format
+            predict_output = predict_mpg(data)
+            return {'predict': predict_output}
+        except Exception as error:
+            return {'error': str(error)}
+
+api.add_resource(Test, '/')
+api.add_resource(GetPredictionOutput, '/getPredictionOutput')
 
 if __name__ == '__main__':
     app.run(debug=True)
